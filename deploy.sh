79ecb72efa1bdf9a5b3fa360351dccfe2c3aecc9
Elliot Forbes <emforce@live.com>
2019-04-21 00:24:39 +0100

    hopefully last change of the night

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 docker push "${IMAGE_NAME}:latest" && docker push "${IMAGE_NAME}:${version}"
 echo "Hello World"
-kubectl --kubeconfig=kube/cluster-config.yaml apply -f ./kube/deployment.yml
-kubectl --kubeconfig=kube/cluster-config.yaml apply -f ./kube/service.yml
+kubectl --kubeconfig="./kube/cluster-config.yaml" apply -f ./kube/deployment.yml
+kubectl --kubeconfig="./kube/cluster-config.yaml" apply -f ./kube/service.yml
