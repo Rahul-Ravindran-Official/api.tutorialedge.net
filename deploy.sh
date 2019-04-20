5a51faf3f04f640be1a97f1dae30b27d229682a8
Elliot Forbes <emforce@live.com>
2019-04-21 00:31:33 +0100

    Urgh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 
 docker push "${IMAGE_NAME}:latest" && docker push "${IMAGE_NAME}:${version}"
 echo "Hello World"
-openssl aes-256-cbc -K $encrypted_e121f330d406_key -iv $encrypted_e121f330d406_iv
-  -in cluster-config.yaml.enc -out kube/cluster-config.yaml -d
+openssl aes-256-cbc -K $encrypted_e121f330d406_key -iv $encrypted_e121f330d406_iv -in cluster-config.yaml.enc -out kube/cluster-config.yaml -d
 ls
 pwd
 kubectl --kubeconfig="kube/cluster-config.yaml" apply -f ./kube/deployment.yml
