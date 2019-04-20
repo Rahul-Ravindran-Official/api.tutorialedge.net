63f01560593445e383395e5d9f63cb25ae45de4d
Elliot Forbes <emforce@live.com>
2019-04-21 00:21:59 +0100

    Updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 
 docker push "${IMAGE_NAME}:latest" && docker push "${IMAGE_NAME}:${version}"
-./kubectl
 echo "Hello World"
-./kubectl --kubeconfig=kube/cluster-config.yaml apply -f ./kube/deployment.yml
-./kubectl --kubeconfig=kube/cluster-config.yaml apply -f ./kube/service.yml
+kubectl --kubeconfig=kube/cluster-config.yaml apply -f ./kube/deployment.yml
+kubectl --kubeconfig=kube/cluster-config.yaml apply -f ./kube/service.yml
