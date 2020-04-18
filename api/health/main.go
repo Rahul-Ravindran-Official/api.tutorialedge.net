6e728f8452dc0a836b29cf382de8d7bed2edde2e
Elliot Forbes <emforce@live.com>
2020-04-18 11:26:22 +0100

    Testing production deploy

--- api/health/main.go
+++ api/health/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,8 +12,10 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	fmt.Printf("%+v\n", request)
 
+	buildNumber := os.Getenv("TRAVIS_BUILD_NUMBER")
+
 	return events.APIGatewayProxyResponse{
-		Body:       "{ \"status\": \"up\" }",
+		Body:       "{ \"status\": \"up\", \"version\": \"" + buildNumber + "\"}",
 		StatusCode: 200,
 	}, nil
 }
