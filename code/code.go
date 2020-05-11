2be1575b357a901f013b9e5b8f91f72555b68f43
Elliot Forbes <emforce@live.com>
2020-05-11 21:01:04 +0100

    Updates

--- code/code.go
+++ code/code.go
@@ -27,7 +27,7 @@ func ExecuteCode(request events.APIGatewayProxyRequest) (events.APIGatewayProxyR
 	fmt.Println(string(body))
 
 	path := os.Getenv("PATH")
-	os.Setenv("PATH", path+";/var/task/bin")
+	os.Setenv("PATH", path+":/var/task/bin")
 
 	out, _ := exec.Command("env").Output()
 	fmt.Println(string(out))
