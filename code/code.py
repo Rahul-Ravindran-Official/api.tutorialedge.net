c4ea08c546aa78570ba1c52edacd2cff4c97668b
Elliot Forbes <emforce@live.com>
2020-05-12 10:25:51 +0100

    Updates

--- code/code.py
+++ code/code.py
@@ -3,13 +3,14 @@ import json
 import tempfile
 
 def create_temp_file(event):
-    temp_file = tempfile.NamedTemporaryFile(suffix=".go", encoding="utf-8")
+    temp_file = tempfile.NamedTemporaryFile(suffix=".go")
     with temp_file as fp:
         fp.write(bytes(event["body"], 'utf-8'))
     return temp_file
 
 def run_go_code(temp_file):
-    args = ["./../bin/go", "run", temp_file]
+    args = ["./../bin/go", "run", temp_file.name]
+    args = ["go", "run", temp_file.name]
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
 
@@ -22,3 +23,29 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+# def main():
+#     event = {}
+#     event["body"] = """package main
+
+#     import "fmt"
+
+#     func main() {
+
+#         mymap := make(map[string]int)
+
+#         mymap["elliot"] = 25
+
+#         // we can use this if statement to check to see if 
+#         // a given key "elliot" exists within a map in Go
+#         if _, ok := mymap["elliot"]; ok {
+#             // the key 'elliot' exists within the map
+#             fmt.Println(mymap["elliot"])
+#         }
+#     }""" 
+
+#     status = lambda_handler(event, None)
+#     print(status)
+
+# if __name__ == '__main__':
+#     main()
