08e20fb4d5d1d5aec781d5d4ea47f492e8843162
Elliot Forbes <emforce@live.com>
2020-05-12 09:57:34 +0100

    Updates

--- code/code.py
+++ code/code.py
@@ -1,10 +1,22 @@
 import subprocess
 import json
+import tempfile
 
-def lambda_handler(event, context):
-    popen = subprocess.Popen("./../bin/go", stdout=subprocess.PIPE)
+def create_temp_file(event):
+    temp_file = tempfile.NamedTemporaryFile(suffix=".go")
+    with temp_file as fp:
+        fp.write(event["body"])
+    return temp_file
+
+def run_go_code(temp_file):
+    args = ["./../bin/go", "run", temp_file]
+    popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
 
+
+def lambda_handler(event, context):
+    temp_file = create_temp_file(event)
+    run_go_code(temp_file)
 
     return {
         'statusCode': 200,
