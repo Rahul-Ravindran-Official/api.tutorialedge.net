6385d29da271374edaa9f837d6146a70cba39499
Elliot Forbes <emforce@live.com>
2020-05-21 11:49:22 +0100

    Updates

--- challenge/python.go
+++ challenge/python.go
@@ -1,4 +1,4 @@
-package code
+package challenge
 
 import (
 	"encoding/base64"
@@ -10,66 +10,12 @@ import (
 	"os/exec"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/mholt/archiver/v3"
 )
 
-// CodeResponse contains the response from
-// executing the Go code
-type CodeResponse struct {
-	ExitCode string `json:"exit_code"`
-	Output   string `json:"output"`
-}
-
-// ChallengeRequest takes in the source code
-// from the editor as well as a number of tests
-// which are written into a file and ran
-type ChallengeRequest struct {
-	Code  string          `json:"code"`
-	Tests []ChallengeTest `json:"tests"`
-}
-
-// ChallengeTest is a struct which contains
-// the source code for a test file as well as
-// the metadata such as the test name
-type ChallengeTest struct {
-	Name string `json:"name"`
-	Code string `json:"code"`
-}
-
-func setupGoEnvironment() error {
-	path := os.Getenv("PATH")
-	os.Setenv("PATH", path+":/tmp/go/bin")
-	os.Setenv("GOROOT", "/tmp/go")
-	os.Setenv("GOPATH", "/tmp")
-	os.Setenv("GOCACHE", "/tmp/go-cache")
-
-	if _, err := os.Stat("/tmp/go"); os.IsNotExist(err) {
-		err := os.Mkdir("/tmp/go", 0777)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		// untar ./code/go.tar.gz -> /tmp/go
-		err = archiver.Unarchive("./code/go.tar.gz", "/tmp")
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-
-		// output, err := exec.Command("tar", "-xzf", "./code/go.tar.gz", "-C", "/tmp").CombinedOutput()
-		// if err != nil {
-		// 	fmt.Println("Failed to Execute tar command")
-		// 	fmt.Println(err.Error())
-		// 	fmt.Println(string(output))
-		// 	return err
-		// }
-	}
-	return nil
-}
-
-// ExecuteGoChallenge does the job of taking the Go code that has
+// ExecutePythonChallenge does the job of taking the Go code that has
 // been sent to API from a snippet and executing it before
 // returning the response
-func ExecuteGoChallenge(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func ExecutePythonChallenge(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	fmt.Println("Received body: ", request.Body)
 	body, _ := base64.StdEncoding.DecodeString(request.Body)
 	fmt.Println(string(body))
---
+++ cmd/challenge/challenge.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/elliotforbes/api.tutorialedge.net/challenge"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	fmt.Printf("%+v\n", request)
+
+	if request.HTTPMethod != "POST" {
+		return events.APIGatewayProxyResponse{
+			Body:       "Not Found",
+			StatusCode: 404,
+		}, nil
+	}
+
+	switch request.Path {
+	case "/api/v1/challengego", "/v1/executego":
+		response, _ := challenge.ExecuteGoChallenge(request)
+		return response, nil
+	case "/api/v1/executepython", "/v1/executepython":
+		response, _ := challenge.ExecutePythonChallenge(request)
+		return response, nil
+	default:
+		return events.APIGatewayProxyResponse{
+			Body:       "Invalid HTTP Method",
+			StatusCode: 501,
+		}, nil
+	}
+}
+
+func main() {
+	lambda.Start(handler)
+}
