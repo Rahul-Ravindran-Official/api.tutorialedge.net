0b83ce19094890fa78effa80aa988064d3fe1c05
Elliot Forbes <emforce@live.com>
2020-05-28 12:37:54 +0100

    Updates

--- users/user.go
+++ users/user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/TutorialEdge/api.tutorialedge.net/challenge"
 	"github.com/TutorialEdge/api.tutorialedge.net/comments"
 	"github.com/TutorialEdge/api.tutorialedge.net/email"
 	"github.com/aws/aws-lambda-go/events"
@@ -13,18 +14,17 @@ import (
 // User - A user on TutorialEdge! :D
 type User struct {
 	gorm.Model
-	Name       string             `json:"name"`
-	Sub        string             `json:"sub"`
-	GivenName  string             `json:"given_name"`
-	FamilyName string             `json:"family_name"`
-	Nickname   string             `json:"nickname"`
-	Picture    string             `json:"picture"`
-	Aud        string             `json:"aud"`
-	Locale     string             `json:"locale"`
-	Comments   []comments.Comment `json:"comments"`
-	Challenges []Challenge        `json:"challenges"`
+	Name       string                `json:"name"`
+	Sub        string                `json:"sub"`
+	GivenName  string                `json:"given_name"`
+	FamilyName string                `json:"family_name"`
+	Nickname   string                `json:"nickname"`
+	Picture    string                `json:"picture"`
+	Aud        string                `json:"aud"`
+	Locale     string                `json:"locale"`
+	Comments   []comments.Comment    `json:"comments"`
+	Challenges []challenge.Challenge `json:"challenges"`
 }
-
 
 // Result
 type Result struct {
@@ -47,7 +47,7 @@ func GetUser(request events.APIGatewayProxyRequest, db *gorm.DB) (events.APIGate
 	db.Where("author_id = ?", sub).Find(&comments)
 	fmt.Printf("%+v\n", comments)
 
-	var challenges []Challenge
+	var challenges []challenge.Challenge
 	db.Where("author_id = ?", sub).Find(&challenges)
 	fmt.Printf("%+v\n", challenges)
 
